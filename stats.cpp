e9418567d1edd05d0065ee0cb77d9c816f389e1e
deekshaarora12 <68547341+deekshaarora12@users.noreply.github.com>
2020-09-02 19:52:43 +0530

    Update stats.cpp

--- stats.cpp
+++ stats.cpp
@@ -1,7 +1,7 @@
 #include "stats.h"
 
 Stats Statistics::ComputeStatistics(const std::vector<float> &mystats) {
-    struct computedStats cs;
+    struct Stats cs;
     int n = mystats.size();
     int sum = mystats[0];
 	
