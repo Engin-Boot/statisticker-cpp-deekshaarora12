097d828f4134a4229e72bdd440855ea154a9fc34
deekshaarora12 <68547341+deekshaarora12@users.noreply.github.com>
2020-09-02 19:51:17 +0530

    Update stats.cpp

--- stats.cpp
+++ stats.cpp
@@ -1,21 +1,14 @@
 #include "stats.h"
 
-Stats Statistics::ComputeStatistics(const std::vector<float> &mystats ) {
+Stats Statistics::ComputeStatistics(const std::vector<float> &mystats) {
     struct computedStats cs;
     int n = mystats.size();
     int sum = mystats[0];
+	
+    sort(mystats.begin(), mystats.end()); 
     
-    for(int i=1; i<n; i++){
-        sum=sum+mystats[i];
-        for (j = 0; j < (n - 1); j++)
-		{
-			if (mystats[j + 1] < mystats[j])				
-			{
-				temp = mystats[j];								
-				mystats[j] = mystats[j + 1];
-				mystats[j + 1] = temp;
-			}
-		}
+    for(int i = 0; i < n; i++){
+        sum = sum + mystats[i];
     }
     cs.max = mystats[n-1];
     cs.min = mystats[0];
