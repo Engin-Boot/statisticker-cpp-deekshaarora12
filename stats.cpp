b76544e5c05bbb87d0f413836c6f4db76234c03a
deekshaarora12 <68547341+deekshaarora12@users.noreply.github.com>
2020-09-03 19:27:38 +0530

    Update stats.cpp

--- stats.cpp
+++ stats.cpp
@@ -9,7 +9,7 @@ using namespace Statistics;
 	
    auto max_ele = *max_element(std::begin(mystats), std::end(mystats));
    auto min_ele = *min_element(std::begin(mystats), std::end(mystats));
-   auto  avg = accumulate(std::begin(mystats), std::end(mystats), 0.0) / n; 
+   auto  avg = accumulate(std::begin(mystats), std::end(mystats)) / n; 
 	
     ds.max = max_ele;
     ds.min = min_ele;
