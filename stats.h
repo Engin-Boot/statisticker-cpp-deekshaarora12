c93ce051f6eff4d1f88b5e256a449c4d5dca3d72
deekshaarora12 <68547341+deekshaarora12@users.noreply.github.com>
2020-09-03 19:21:43 +0530

    Update stats.h

--- stats.h
+++ stats.h
@@ -1,11 +1,12 @@
 #include <vector>
+#include<math.h>
 
 namespace Statistics {
        
  struct declareStats{
-    float average;
-    float max;
-    float min;
+    float average = Nan;
+    float max = NaN;
+    float min = NaN;
 };  
    extern struct declareStats ds;
    declareStats ComputeStatistics(const std::vector<float> &mystats);
