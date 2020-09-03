282884c8c8a4854544ea51bf8b12e2ded1ffa09a
deekshaarora12 <68547341+deekshaarora12@users.noreply.github.com>
2020-09-03 20:04:37 +0530

    Update stats.cpp

--- stats.cpp
+++ stats.cpp
@@ -5,21 +5,18 @@ using namespace Statistics;
 
  declareStats Statistics::ComputeStatistics(const std::vector<float> &mystats) {
     Statistics::declareStats ds;
-    float sum = 0.0;
+    float sum = 0;
     int n = mystats.size();
 	
    auto max_ele = *max_element(std::begin(mystats), std::end(mystats));
    auto min_ele = *min_element(std::begin(mystats), std::end(mystats));
    auto  avg = accumulate(std::begin(mystats), std::end(mystats),sum) / n; 
-    for(auto& it : mystats){
-	   sum = sum + it;
-}
     
-f(mystats.size()!=0){
+if(mystats.size()!=0){
     ds.max = max_ele;
     ds.min = min_ele;
     ds.average = avg;
-}
-	 
+    return ds;
+}	 
     return ds;
 }
