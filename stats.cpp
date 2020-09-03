b5d9d1a542e95cea9c10054cb7df342046f2d92c
deekshaarora12 <68547341+deekshaarora12@users.noreply.github.com>
2020-09-03 20:37:41 +0530

    Update stats.cpp

--- stats.cpp
+++ stats.cpp
@@ -1,18 +1,16 @@
 #include "stats.h"
 #include <bits/stdc++.h> 
 
-struct declareStats;
+struct declareStats ds;
 
  declareStats ComputeStatistics(const std::vector<float> &mystats) {
-   Statistics::declareStats ds;
     float sum = 0.0;
     int n = mystats.size();
 	 
    auto max_ele = *max_element(std::begin(mystats), std::end(mystats));
    auto min_ele = *min_element(std::begin(mystats), std::end(mystats));
    auto  avg = accumulate(std::begin(mystats), std::end(mystats),sum) / n; 
-    
-
+	 
     ds.max = max_ele;
     ds.min = min_ele;
     ds.average = avg;
