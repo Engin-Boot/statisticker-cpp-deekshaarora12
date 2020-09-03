fdb7fd7cf7a08ae865f728015cc36fffda2a6ea1
deekshaarora12 <68547341+deekshaarora12@users.noreply.github.com>
2020-09-03 12:09:50 +0530

    Update stats.cpp

--- stats.cpp
+++ stats.cpp
@@ -5,8 +5,8 @@ declareStats Statistics::ComputeStatistics(const std::vector<float> &mystats) {
     int n = mystats.size();
     float sum = mystats[0];
 	
-   auto max_ele = <float>max_element(std::begin(mystats), std::end(mystats));
-   auto min_ele = <float>min_element(std::begin(mystats), std::end(mystats));
+   auto max_ele = *max_element(std::begin(mystats), std::end(mystats));
+   auto min_ele = *min_element(std::begin(mystats), std::end(mystats));
 	
     for(int i = 1; i < n; i++){
         sum = sum + mystats[i];
