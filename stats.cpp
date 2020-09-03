6b574e7e3d01d5c95481faf03d8c7f3f336f39b4
deekshaarora12 <68547341+deekshaarora12@users.noreply.github.com>
2020-09-03 19:35:42 +0530

    Update stats.cpp

--- stats.cpp
+++ stats.cpp
@@ -12,7 +12,8 @@ using namespace Statistics;
    auto min_ele = *min_element(std::begin(mystats), std::end(mystats));
    //auto  avg = accumulate(std::begin(mystats), std::end(mystats),0.0) / n; 
     for(int i=0;i<n;i++){
-	sum = sum + mystats[i];
+	if(mystats.size()!=0)
+	   sum = sum + mystats[i];
 }
 	
     ds.max = max_ele;
