0bb7bfdfeff3843b15542087e6d60cdac07ba488
deekshaarora12 <68547341+deekshaarora12@users.noreply.github.com>
2020-09-03 13:10:00 +0530

    Update stats.cpp

--- stats.cpp
+++ stats.cpp
@@ -1,7 +1,7 @@
 #include "stats.h"
 #include <bits/stdc++.h> 
 
-declareStats Statistics::ComputeStatistics(const std::vector<float> &mystats) {
+ Statistics::declareStats ComputeStatistics(const std::vector<float> &mystats) {
     int n = mystats.size();
     float sum = mystats[0];
 	
