4baad110264a6eab09881372c3e66dc850ee84db
deekshaarora12 <68547341+deekshaarora12@users.noreply.github.com>
2020-09-03 19:24:35 +0530

    Update stats.h

--- stats.h
+++ stats.h
@@ -4,9 +4,9 @@
 namespace Statistics {
        
  struct declareStats{
-    float average = Nan;
-    float max = NaN;
-    float min = NaN;
+    float average = std::numeric_limits<float>::quiet_NaN();
+    float max = std::numeric_limits<float>::quiet_NaN();
+    float min = std::numeric_limits<float>::quiet_NaN();
 };  
    extern struct declareStats ds;
    declareStats ComputeStatistics(const std::vector<float> &mystats);
