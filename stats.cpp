cb5eabfd2badbab755ea5304b3298a5290e71044
deekshaarora12 <68547341+deekshaarora12@users.noreply.github.com>
2020-09-03 12:01:14 +0530

    Update stats.cpp

--- stats.cpp
+++ stats.cpp
@@ -5,13 +5,12 @@ declareStats Statistics::ComputeStatistics(const std::vector<float> &mystats) {
     int n = mystats.size();
     float sum = mystats[0];
 	
-   std::sort(mystats.begin(),mystats.end());
+    ds.max = max_element(std::begin(mystats), std::end(mystats));
+    ds.min = min_element(std::begin(mystats), std::end(mystats));
 	
     for(int i = 1; i < n; i++){
         sum = sum + mystats[i];
  }
-    ds.max = mystats[n-1];
-    ds.min = mystats[0];
     ds.average= sum/n;
     return ds;
 }
